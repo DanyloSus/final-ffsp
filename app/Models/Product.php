380681a25e4daee8fb4607c90536558dca257153
DanyloSus <danil.sushko19@gmail.com>
2024-04-24 14:53:54 +0300

    relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     use HasFactory;
+
+    private $fillable = ['name', 'description'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categorie(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- database/migrations/2024_04_24_114530_create_products_table.php
+++ database/migrations/2024_04_24_114530_create_products_table.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +14,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->text('description')->nullable();
+
+            $table->foreignIdFor(Category::class);
+            $table->foreignIdFor(User::class);
+
             $table->timestamps();
         });
     }
